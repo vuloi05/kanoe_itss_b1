e2a069acd9088ec6b7fe2f650e23467cc8e75576
TungSoYeu <tungnghienhy@gmail.com>
2026-06-16 01:23:54 +0700

    fix: remove extra closing div in ChatArea causing build error

--- frontend/src/app/partner/messages/components/ChatArea.tsx
+++ frontend/src/app/partner/messages/components/ChatArea.tsx
@@ -180,7 +180,6 @@ export default function ChatArea({
                     </div>
                   </div>
                 </div>
-                </div>
               </div>
             );
           }
